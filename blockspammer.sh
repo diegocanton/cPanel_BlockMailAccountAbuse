e7dcd020d9fb79e631956c522cddfc35fb06596c
Diego Canton de Brito <diegocantonbgi@gmail.com>
2018-02-02 01:00:40 -0200

    Fix
    Corrigido erro na ordem de execução e uso de comandos

--- blockspammer.sh
+++ blockspammer.sh
@@ -15,19 +15,17 @@ exiqgrep -y 3600 | egrep "<*@.+>" | awk '{print $4}' | sort | uniq -c | sort -n 
 
 ### As regras a seguir pegam as contas que identificamos anteriormente e suspendem o login e envio de novas mensagens
 ## Ler o arquivo gerado anteriormente, gera um comando de API do WHM para obter o Usuario, seguido de executar o comando de suspensao de login e bloquear a fila
-cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email suspend_login email=`cat cat /tmp/suspenderconta`" | sh
-cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email hold_outgoing email=`cat cat /tmp/suspenderconta`" | sh
+cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email suspend_login email="`cat /tmp/suspenderconta` | sh
+cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email hold_outgoing email="`cat /tmp/suspenderconta` | sh
+
 ## Ler o arquivo gerado anteriormente e suspender o envio de mensagens pelo usuario, isso impede que um usuario ja logado continue a enviar mensagens
 cat /tmp/suspenderconta | xargs -I '{}' whmapi1 suspend_outgoing_email user={}
 
 ### Reiniciamos o exim para termos certeza que a conta foi bloqueada
 /scripts/restartsrv_exim
 
+### Envia e-mail para um email alertando o bloqueio
+echo "Bloqueamos o usuario por ter enviado mais de 100 mensagens na ultima hora para destinos que negaram a recepcao." | mail -s "Usuario "`cat /tmp/suspenderconta`" bloqueado em "`hostname`" por envio de SPAM" <email>
+
 ### Removemos o arquivo para não voltar a suspender o cliente após resolver a problema
 rm -rf /tmp/suspenderconta
-
-### Envia e-mail para um email alertando o bloqueio
-echo "Bloqueamos o usuario por ter enviado mais de 100 mensagens na ultima hora para destinos que negaram a recepcao." | mail -s "Usuario `cat /tmp/suspenderconta` bloqueado em `hostname` por envio de SPAM" <email>;
-
-
-
