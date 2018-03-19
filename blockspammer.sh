2586f8258b37bf6a7d1ee4be8ad0a8a8e125a43f
Diego Canton de Brito <diegocantonbgi@gmail.com>
2018-03-18 21:52:31 -0300

    Update blockspammer.sh
    Primeiro teste do script.
    Foi alterado o parâmetro de checagem da queue de 1 hora para 1 dia e editado a expressão regular que identifica os envios, de 100 por hora para "mais de 100" por hora
    
    Removido um paramentro da API do WHM, pois server para suspender o usuário cPanel e não a conta de e-mail.

--- blockspammer.sh
+++ blockspammer.sh
@@ -4,28 +4,29 @@
 whmapi1 listaccts  | grep domain | awk -F ":" '{print $2}' | sed "s/ /@/g" > /tmp/domainslocal
 
 ### Regra explicada cada parte entre | em uma linha
-## Lista mensagens enviadas na última 1 hora
+## Lista mensagens na fila na última hora
 ## Filtra apenas as linhas que contenham um e-mail válido
 ## Filtramos apenas o conteúdo dos e-mails que estão na 4 coluna
 ## Resumindo 3 comandos: Organiza de forma crescente, contabiliza os repetidos e reorganiza crescente
 ## Filtramos os remetentes com mais de 100 mensagens
 ## Resumindo 2 comandos: Ignoramos a quantidade e os caracteres "<" e ">" para usarmos apenas os e-mails
 ## Comparamos se os remetentes contem o dominios existentes no servidor (a lista que obtivemos no comando anterior)
-exiqgrep -y 3600 | egrep "<*@.+>" | awk '{print $4}' | sort | uniq -c | sort -n | egrep "1[0-9]{1,2} <" | awk -F "<" '{print $2}' | awk -F ">" '{print $1}' | grep -f /tmp/domainslocal > /tmp/suspenderconta
+exiqgrep -y 3600 | egrep "<*@.+>" | awk '{print $4}' | sort | uniq -c | sort -n | egrep "[0-9]{1}[0-9]{2,9} <" | awk -F "<" '{print $2}' | awk -F ">" '{print $1}' | grep -f /tmp/domainslocal > /tmp/suspenderconta
 
 ### As regras a seguir pegam as contas que identificamos anteriormente e suspendem o login e envio de novas mensagens
 ## Ler o arquivo gerado anteriormente, gera um comando de API do WHM para obter o Usuario, seguido de executar o comando de suspensao de login e bloquear a fila
+echo "Suspendendo `cat /tmp/suspenderconta`"
+cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email suspend_login email="`cat /tmp/suspenderconta`
 cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email suspend_login email="`cat /tmp/suspenderconta` | sh
-cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email hold_outgoing email="`cat /tmp/suspenderconta` | sh
 
-## Ler o arquivo gerado anteriormente e suspender o envio de mensagens pelo usuario, isso impede que um usuario ja logado continue a enviar mensagens
-cat /tmp/suspenderconta | xargs -I '{}' whmapi1 suspend_outgoing_email user={}
+cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email hold_outgoing email="`cat /tmp/suspenderconta`
+cat /tmp/suspenderconta | awk -F "@" '{print "whmapi1 listaccts search="$2" searchtype=domain | grep user" }' | sh | awk -F ": " '{print $2}' | xargs -I '{}' echo "uapi --user={} Email hold_outgoing email="`cat /tmp/suspenderconta` | sh
 
 ### Reiniciamos o exim para termos certeza que a conta foi bloqueada
 /scripts/restartsrv_exim
 
 ### Envia e-mail para um email alertando o bloqueio
-echo "Bloqueamos o usuario por ter enviado mais de 100 mensagens na ultima hora para destinos que negaram a recepcao." | mail -s "Usuario "`cat /tmp/suspenderconta`" bloqueado em "`hostname`" por envio de SPAM" <email>
+echo "Bloqueamos o usuario por ter enviado mais de 100 mensagens na ultima hora para destinos que negaram a recepcao." | mail -s "Usuario "`cat /tmp/suspenderconta`" bloqueado em "`hostname`" por envio de SPAM" suporte@ensite.com.br
 
 ### Removemos o arquivo para não voltar a suspender o cliente após resolver a problema
 rm -rf /tmp/suspenderconta
