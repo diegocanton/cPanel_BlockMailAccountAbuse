cdd2391bb34c77c9c767f7e757c8ef25cdc7d1e8
Diego Canton de Brito <diegocantonbgi@gmail.com>
2018-07-16 19:48:43 -0300

    Update PrefixReject.sh

--- PrefixReject.sh
+++ PrefixReject.sh
@@ -5,4 +5,8 @@
 # Apenas lista contas que receberam negação com códigos 4.7.x e 5.7.x
 # 
 
-exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | grep -Poe '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )' | sort -n | uniq -c
+## Localiza os logins do log todo
+#exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | grep -Poe '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )' | sort -n | uniq -c
+
+## Localiza os logins da última hora
+exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | awk '($0 >= from)' from="$(LC_ALL=C date +'%Y-%m-%d %H:%M:%S' -d -1hour)" | grep -Poe '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )'
