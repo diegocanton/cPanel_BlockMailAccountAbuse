7c4a20e6b4af5c7fb56a662f0281f1a70fddef84
Diego Canton de Brito <diegocantonbgi@gmail.com>
2018-07-17 20:10:16 -0300

    Update PrefixReject.sh

--- PrefixReject.sh
+++ PrefixReject.sh
@@ -9,6 +9,15 @@
 #exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | grep -Poe '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )' | sort -n | uniq -c
 
 ## Localiza os logins da última hora
-exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | awk '($0 >= from)' from="$(LC_ALL=C date +'%Y-%m-%d %H:%M:%S' -d -1hour)" | grep -Poe '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )'
+## EXEMPLOS:
+## Exim + cPanel
+exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | awk '($0 >= from)' from="$(LC_ALL=C date +'%Y-%m-%d %H:%M:%S' -d -1hour)" | grep -Po '(?<=(dovecot_login|dovecot_plain):)[^ ]+(?= )' | sort | uniq -c | sort
+
+## Exim
+exigrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/exim_mainlog | awk '($0 >= from)' from="$(LC_ALL=C date +'%Y-%m-%d %H:%M:%S' -d -1hour)" | grep -Po '(?<=plain_server:)[^ ]+(?= )' | sort | uniq -c | sort
+
+## POSTFIX + Zimbra
+egrep ": [0-9]{1,3} [4|5]\.7\.[0-9] " /var/log/mail.log | awk '($0 >= from)' from="$(LC_ALL=C date +'%b %d %H:%M:%S' -d -1hour)" |  grep -Po '(?<=from=<)[^>]+(?=>)' | sort | uniq -c | sort
+
 
 #
